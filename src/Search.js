2fabee934d5023066f8059a3bb531e024a546214
sdfsfd <sdfd@sdfsd.ff>
2018-11-25 15:12:54 +0300

    Azumre VM results

--- src/Search.js
+++ src/Search.js
@@ -5,21 +5,21 @@ class Search extends PureComponent {
     super(props);
 
     this.textChanged = this.textChanged.bind(this);
-    this.state = {text: "", blink:false};
+    this.state = {extraSpaces: true, blink:false};
     this.textInput = React.createRef();
   }
 
   textChanged(e){
-    //e.preventDefault();
     if (this.props.searchChanged) this.props.searchChanged(e.target.innerText);
-    //this.setState({text: e.value});
+    if (e.target.innerText && this.state.extraSpaces) this.setState({extraSpaces: false});
+    else if (!e.target.innerText && !this.state.extraSpaces) this.setState({extraSpaces: true});
   }
 
   render() {
     return (
       <div className="searchBox">
         <span contentEditable="true" ref={this.textInput} onInput={this.textChanged} onFocus={() => {this.setState({blink:true})}} onBlur={() => {this.setState({blink:false})}}></span>
-        <span className={this.state.blink ? "blink" : ""} onClick={() => {this.textInput.current.focus()}}>{!this.state.blink && "\xA0\xA0"}_</span>
+        <span className={this.state.blink ? "blink" : ""} onClick={() => {this.textInput.current.focus()}}>{!this.state.blink && this.state.extraSpaces && "\xA0\xA0"}_</span>
         &lt;/
       </div>
     )
