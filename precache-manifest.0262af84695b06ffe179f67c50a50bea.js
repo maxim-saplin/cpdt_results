65cb7d844ffa138833ad509033292c5365f7d56f
Maxim Saplin <smaxmail@gmail.com>
2018-11-26 10:54:05 +0300

    Updates

--- precache-manifest.0262af84695b06ffe179f67c50a50bea.js
+++ precache-manifest.0262af84695b06ffe179f67c50a50bea.js
@@ -16,19 +16,19 @@ self.__precacheManifest = [
     "url": "/cpdt_results/static/js/runtime~main.95b7c02d.js"
   },
   {
-    "revision": "0abc8212ad2b7b908794",
-    "url": "/cpdt_results/static/js/main.0abc8212.chunk.js"
+    "revision": "ed78ab039cf762551391",
+    "url": "/cpdt_results/static/js/main.ed78ab03.chunk.js"
   },
   {
     "revision": "a93cec8a557ee382682a",
     "url": "/cpdt_results/static/js/1.a93cec8a.chunk.js"
   },
   {
-    "revision": "0abc8212ad2b7b908794",
-    "url": "/cpdt_results/static/css/main.5bfd3587.chunk.css"
+    "revision": "ed78ab039cf762551391",
+    "url": "/cpdt_results/static/css/main.f56248b0.chunk.css"
   },
   {
-    "revision": "7cd51d0bb0bf8890fbfd8d8326f45808",
+    "revision": "7d10b7903304c3a42aa34cda37d37fc8",
     "url": "/cpdt_results/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cpdt_results/precache-manifest.3759ffaf821d4f4463c3c28804d14444.js"
+  "/cpdt_results/precache-manifest.0262af84695b06ffe179f67c50a50bea.js"
 );
 
 workbox.clientsClaim();
