8a11f91a5aea308e98c3a853abd0136de08f3a62
sdfsfd <sdfd@sdfsd.ff>
2018-12-14 21:03:03 +0300

    Updates

---
+++ precache-manifest.3cfd5a30d5d9004c92f4401ce67dbfa6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "6624977e05316ed6e27ee2a32738ab7e",
+    "url": "/cpdt_results/static/media/IBMPlexMono-SemiBold.6624977e.woff2"
+  },
+  {
+    "revision": "3e6fa0a0aefa88d0d2c4",
+    "url": "/cpdt_results/static/css/main.d165ca40.chunk.css"
+  },
+  {
+    "revision": "0daae5422eb6b8249777",
+    "url": "/cpdt_results/static/js/1.0daae542.chunk.js"
+  },
+  {
+    "revision": "95b7c02d66ede0201f4c",
+    "url": "/cpdt_results/static/js/runtime~main.95b7c02d.js"
+  },
+  {
+    "revision": "d55f91bf0056794f4d2ad74327011e67",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Medium.d55f91bf.woff2"
+  },
+  {
+    "revision": "27a3afd6a029d53608f80f2ee809f614",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Regular.27a3afd6.woff2"
+  },
+  {
+    "revision": "3e6fa0a0aefa88d0d2c4",
+    "url": "/cpdt_results/static/js/main.3e6fa0a0.chunk.js"
+  },
+  {
+    "revision": "036d007395279645d7dd332ac08e6c66",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Italic.036d0073.woff2"
+  },
+  {
+    "revision": "5632af57e4d486d7597865250c6b1e77",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Regular.5632af57.woff"
+  },
+  {
+    "revision": "70b59c39c9071242baa75908c9796d0e",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Medium.70b59c39.woff"
+  },
+  {
+    "revision": "37698cdd304ef06da08b86889e5593b5",
+    "url": "/cpdt_results/static/media/IBMPlexMono-SemiBold.37698cdd.woff"
+  },
+  {
+    "revision": "97aeea180bd7d5694ecdb8998ea9d275",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Italic.97aeea18.woff"
+  },
+  {
+    "revision": "e60a8b53ba161f40af321e5bcf4ffa8f",
+    "url": "/cpdt_results/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cpdt_results/precache-manifest.534cc5eac2e81337d3ae2cb973fbc412.js"
+  "/cpdt_results/precache-manifest.3cfd5a30d5d9004c92f4401ce67dbfa6.js"
 );
 
 workbox.clientsClaim();
