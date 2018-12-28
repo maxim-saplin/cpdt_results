19ada3376ef10dc8e7d400f14410e3ecd1de121e
sdfsfd <sdfd@sdfsd.ff>
2018-12-28 20:12:33 +0300

    Updates

--- precache-manifest.6ba6c5f24ed2e59ef328c58c40c34873.js
+++ precache-manifest.6ba6c5f24ed2e59ef328c58c40c34873.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/cpdt_results/static/media/IBMPlexMono-SemiBold.6624977e.woff2"
   },
   {
-    "revision": "471bcb20456a34a84dbe",
-    "url": "/cpdt_results/static/css/main.b0c9388e.chunk.css"
+    "revision": "725b4b378afdacb1bf2f",
+    "url": "/cpdt_results/static/css/main.738d306d.chunk.css"
   },
   {
     "revision": "0daae5422eb6b8249777",
@@ -16,16 +16,16 @@ self.__precacheManifest = [
     "url": "/cpdt_results/static/js/runtime~main.95b7c02d.js"
   },
   {
-    "revision": "d55f91bf0056794f4d2ad74327011e67",
-    "url": "/cpdt_results/static/media/IBMPlexMono-Medium.d55f91bf.woff2"
-  },
-  {
     "revision": "27a3afd6a029d53608f80f2ee809f614",
     "url": "/cpdt_results/static/media/IBMPlexMono-Regular.27a3afd6.woff2"
   },
   {
-    "revision": "471bcb20456a34a84dbe",
-    "url": "/cpdt_results/static/js/main.471bcb20.chunk.js"
+    "revision": "d55f91bf0056794f4d2ad74327011e67",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Medium.d55f91bf.woff2"
+  },
+  {
+    "revision": "725b4b378afdacb1bf2f",
+    "url": "/cpdt_results/static/js/main.725b4b37.chunk.js"
   },
   {
     "revision": "036d007395279645d7dd332ac08e6c66",
@@ -48,7 +48,7 @@ self.__precacheManifest = [
     "url": "/cpdt_results/static/media/IBMPlexMono-SemiBold.37698cdd.woff"
   },
   {
-    "revision": "622ce3c935ebecff2f78d7a6ae42bdec",
+    "revision": "e6aa7f912f0e1e0cac243583e8536b47",
     "url": "/cpdt_results/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cpdt_results/precache-manifest.8a1cf55031b629378c5d3a08de8e4c31.js"
+  "/cpdt_results/precache-manifest.6ba6c5f24ed2e59ef328c58c40c34873.js"
 );
 
 workbox.clientsClaim();
