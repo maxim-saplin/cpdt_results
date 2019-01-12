9a6ea83b073b660fbf20f51b6a0e7feb377ce095
sdfsfd <sdfd@sdfsd.ff>
2019-01-13 02:05:18 +0300

    Updates

--- precache-manifest.c3f656b79bd704f5299bb9da38a3dfc8.js
+++ precache-manifest.c3f656b79bd704f5299bb9da38a3dfc8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "6624977e05316ed6e27ee2a32738ab7e",
-    "url": "/cpdt_results/static/media/IBMPlexMono-SemiBold.6624977e.woff2"
+    "revision": "036d007395279645d7dd332ac08e6c66",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Italic.036d0073.woff2"
   },
   {
-    "revision": "5319eb6487dc928cef42",
+    "revision": "6afd4b21764b910d381e",
     "url": "/cpdt_results/static/css/main.bfa2397b.chunk.css"
   },
   {
@@ -44,36 +44,36 @@ self.__precacheManifest = [
     "url": "/cpdt_results/static/media/IBMPlexMono-Medium.d55f91bf.woff2"
   },
   {
-    "revision": "5319eb6487dc928cef42",
-    "url": "/cpdt_results/static/js/main.5319eb64.chunk.js"
+    "revision": "6afd4b21764b910d381e",
+    "url": "/cpdt_results/static/js/main.6afd4b21.chunk.js"
   },
   {
-    "revision": "036d007395279645d7dd332ac08e6c66",
-    "url": "/cpdt_results/static/media/IBMPlexMono-Italic.036d0073.woff2"
-  },
-  {
-    "revision": "5632af57e4d486d7597865250c6b1e77",
-    "url": "/cpdt_results/static/media/IBMPlexMono-Regular.5632af57.woff"
+    "revision": "6624977e05316ed6e27ee2a32738ab7e",
+    "url": "/cpdt_results/static/media/IBMPlexMono-SemiBold.6624977e.woff2"
   },
   {
     "revision": "70b59c39c9071242baa75908c9796d0e",
     "url": "/cpdt_results/static/media/IBMPlexMono-Medium.70b59c39.woff"
   },
   {
-    "revision": "bd379b4108d626229acca4a4cf8f66b6",
-    "url": "/cpdt_results/static/media/RuBlack.bd379b41.png"
-  },
-  {
     "revision": "dd31905cb4394b5965adae9cb2bfd25c",
     "url": "/cpdt_results/static/media/EnBlack.dd31905c.png"
   },
   {
-    "revision": "97aeea180bd7d5694ecdb8998ea9d275",
-    "url": "/cpdt_results/static/media/IBMPlexMono-Italic.97aeea18.woff"
+    "revision": "bd379b4108d626229acca4a4cf8f66b6",
+    "url": "/cpdt_results/static/media/RuBlack.bd379b41.png"
   },
   {
     "revision": "37698cdd304ef06da08b86889e5593b5",
     "url": "/cpdt_results/static/media/IBMPlexMono-SemiBold.37698cdd.woff"
+  },
+  {
+    "revision": "5632af57e4d486d7597865250c6b1e77",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Regular.5632af57.woff"
+  },
+  {
+    "revision": "97aeea180bd7d5694ecdb8998ea9d275",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Italic.97aeea18.woff"
   },
   {
     "revision": "47b2b792238f5cf6b4a26b91f1e65456",
@@ -84,7 +84,7 @@ self.__precacheManifest = [
     "url": "/cpdt_results/static/media/EnWhite.bebad3b9.png"
   },
   {
-    "revision": "b4ad79e6964c8e678b23948581a2a84a",
+    "revision": "3096a54fa8147da16008143f7c1d3044",
     "url": "/cpdt_results/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cpdt_results/precache-manifest.a3aec8ef16e9b376976ae2aac198006c.js"
+  "/cpdt_results/precache-manifest.c3f656b79bd704f5299bb9da38a3dfc8.js"
 );
 
 workbox.clientsClaim();
