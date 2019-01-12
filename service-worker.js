58add4ff79d7ce7c43ecfc7f22a06a5d612eb5b8
sdfsfd <sdfd@sdfsd.ff>
2019-01-13 00:48:18 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cpdt_results/precache-manifest.76cf314c36e27d44d10ba56ce2ab5c88.js"
+  "/cpdt_results/precache-manifest.47f3b3e24b4d83b3ec987b75db0ccd83.js"
 );
 
 workbox.clientsClaim();
