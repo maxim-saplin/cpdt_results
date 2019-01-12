b8dc6c22c8cf2d92b03a7a9f9998288d0ca2c9da
sdfsfd <sdfd@sdfsd.ff>
2019-01-13 01:27:43 +0300

    ANothe fix

--- src/DownloadLinks.js
+++ src/DownloadLinks.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import windows from './img/windows.svg';
+import macOS from './img/macos.svg';
+import google_play from './img/google-play.svg';
+import apk from './img/apk.svg';
+import github from './img/github.svg';
 
 export default function DownloadLinks(props){
     return (
         <>
-            <a href="https://github.com/maxim-saplin/CrossPlatformDiskTest/releases/download/2.0.0/CPDT.exe" target="_blank"><img src="img/windows.svg"/><br/>Windows</a>
-            <a href="https://github.com/maxim-saplin/CrossPlatformDiskTest/releases/download/2.0.0/CPDT.app.zip" target="_blank"><img src="img/macos.svg"/><br/>macOS</a>
-            <a href="https://play.google.com/store/apps/details?id=com.Saplin.CPDT" target="_blank"><img src="img/google-play.svg"/><br/>Android</a>
-            <a href="https://github.com/maxim-saplin/CrossPlatformDiskTest/releases/download/2.0.1/com.Saplin.CPDT.apk" target="_blank"><img src="img/apk.svg"/><br/>.APK</a>
-            <a href="https://github.com/maxim-saplin/CrossPlatformDiskTest" target="_blank"><img src="img/github.svg"/><br/>GitHub</a>
+            <a href="https://github.com/maxim-saplin/CrossPlatformDiskTest/releases/download/2.0.0/CPDT.exe" 
+            target="_blank">
+                <img src={windows}/>
+                <br/>Windows
+            </a>
+            <a href="https://github.com/maxim-saplin/CrossPlatformDiskTest/releases/download/2.0.0/CPDT.app.zip" 
+            target="_blank">
+                <img src={macOS}/>
+                <br/>macOS
+            </a>
+            <a href="https://play.google.com/store/apps/details?id=com.Saplin.CPDT" 
+            target="_blank">
+                <img src={google_play}/><br/>
+                Android
+            </a>
+            <a href="https://github.com/maxim-saplin/CrossPlatformDiskTest/releases/download/2.0.1/com.Saplin.CPDT.apk" 
+            target="_blank">
+                <img src={apk}/>
+                <br/>.APK
+            </a>
+            <a href="https://github.com/maxim-saplin/CrossPlatformDiskTest" 
+            target="_blank">
+                <img src={github}/>
+                <br/>GitHub
+            </a>
         </>
     )
 }
