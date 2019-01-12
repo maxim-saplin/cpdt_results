e302d94e5bcfa928ccf7eb161c63e6e6241eb8d9
sdfsfd <sdfd@sdfsd.ff>
2019-01-13 01:28:20 +0300

    Updates

---
+++ precache-manifest.e26a182149a025167eb66973736b1327.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "d55f91bf0056794f4d2ad74327011e67",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Medium.d55f91bf.woff2"
+  },
+  {
+    "revision": "2762e632ef82ab656ec5",
+    "url": "/cpdt_results/static/css/main.5a06cf20.chunk.css"
+  },
+  {
+    "revision": "3b15dfe28f3c09292b7a",
+    "url": "/cpdt_results/static/js/1.3b15dfe2.chunk.js"
+  },
+  {
+    "revision": "95b7c02d66ede0201f4c",
+    "url": "/cpdt_results/static/js/runtime~main.95b7c02d.js"
+  },
+  {
+    "revision": "15e725319135ac27002d0751851a978b",
+    "url": "/cpdt_results/static/media/windows.15e72531.svg"
+  },
+  {
+    "revision": "3a236d88e7bd2ce0636f60bd1dcc5b7d",
+    "url": "/cpdt_results/static/media/macos.3a236d88.svg"
+  },
+  {
+    "revision": "7041ae46fb80f1678b2c020235176f33",
+    "url": "/cpdt_results/static/media/google-play.7041ae46.svg"
+  },
+  {
+    "revision": "a00fa3a4c76271f16b9808841091a2b6",
+    "url": "/cpdt_results/static/media/apk.a00fa3a4.svg"
+  },
+  {
+    "revision": "990c2b02425e4ee8299a18b645f267b5",
+    "url": "/cpdt_results/static/media/github.990c2b02.svg"
+  },
+  {
+    "revision": "27a3afd6a029d53608f80f2ee809f614",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Regular.27a3afd6.woff2"
+  },
+  {
+    "revision": "6624977e05316ed6e27ee2a32738ab7e",
+    "url": "/cpdt_results/static/media/IBMPlexMono-SemiBold.6624977e.woff2"
+  },
+  {
+    "revision": "2762e632ef82ab656ec5",
+    "url": "/cpdt_results/static/js/main.2762e632.chunk.js"
+  },
+  {
+    "revision": "036d007395279645d7dd332ac08e6c66",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Italic.036d0073.woff2"
+  },
+  {
+    "revision": "bd379b4108d626229acca4a4cf8f66b6",
+    "url": "/cpdt_results/static/media/RuBlack.bd379b41.png"
+  },
+  {
+    "revision": "dd31905cb4394b5965adae9cb2bfd25c",
+    "url": "/cpdt_results/static/media/EnBlack.dd31905c.png"
+  },
+  {
+    "revision": "5632af57e4d486d7597865250c6b1e77",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Regular.5632af57.woff"
+  },
+  {
+    "revision": "70b59c39c9071242baa75908c9796d0e",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Medium.70b59c39.woff"
+  },
+  {
+    "revision": "97aeea180bd7d5694ecdb8998ea9d275",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Italic.97aeea18.woff"
+  },
+  {
+    "revision": "37698cdd304ef06da08b86889e5593b5",
+    "url": "/cpdt_results/static/media/IBMPlexMono-SemiBold.37698cdd.woff"
+  },
+  {
+    "revision": "47b2b792238f5cf6b4a26b91f1e65456",
+    "url": "/cpdt_results/static/media/RuWhite.47b2b792.png"
+  },
+  {
+    "revision": "bebad3b9bf225cdecc7b9fe714f3b46d",
+    "url": "/cpdt_results/static/media/EnWhite.bebad3b9.png"
+  },
+  {
+    "revision": "ccedb469901ccea0aa18fc60ad038909",
+    "url": "/cpdt_results/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cpdt_results/precache-manifest.620037eedb91a019e483b2f5dd0f0ff9.js"
+  "/cpdt_results/precache-manifest.e26a182149a025167eb66973736b1327.js"
 );
 
 workbox.clientsClaim();
