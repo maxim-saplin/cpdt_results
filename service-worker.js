913b0284ac6fa5fd7a921c46b6545638c8fea2a2
Maxim Saplin <smaxmail@gmail.com>
2019-08-23 13:33:41 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cpdt_results/precache-manifest.784896c2844163b6cb098d80d3c87eb3.js"
+  "/cpdt_results/precache-manifest.76852112e5550db897086ebbe554a2dd.js"
 );
 
 workbox.clientsClaim();
