aa28c2b52db0a5b45a90869df5b035ed2fafc172
sdfsfd <sdfd@sdfsd.ff>
2019-08-24 13:12:54 +0300

    2.2.7

--- src/DownloadLinks.js
+++ src/DownloadLinks.js
@@ -4,16 +4,17 @@ import macOS from './img/macos.svg';
 import google_play from './img/google-play.svg';
 import apk from './img/apk.svg';
 import github from './img/github.svg';
+import linux from './img/linux.svg';
 
 export default function DownloadLinks(props){
     return (
         <>
-            <a href="https://github.com/maxim-saplin/CrossPlatformDiskTest/releases/download/2.2.5/CPDT.exe.zip" 
+            <a href="https://github.com/maxim-saplin/CrossPlatformDiskTest/releases/download/2.2.7/CPDT.exe.zip" 
             target="_blank">
                 <img src={windows}/>
                 <br/>Windows
             </a>
-            <a href="https://github.com/maxim-saplin/CrossPlatformDiskTest/releases/download/2.2.5/CPDT.app.zip" 
+            <a href="https://github.com/maxim-saplin/CrossPlatformDiskTest/releases/download/2.2.7/CPDT.app.zip" 
             target="_blank">
                 <img src={macOS}/>
                 <br/>macOS
@@ -23,7 +24,7 @@ export default function DownloadLinks(props){
                 <img src={google_play}/><br/>
                 Android
             </a>
-            <a href="https://github.com/maxim-saplin/CrossPlatformDiskTest/releases/download/2.2.5/com.Saplin.CPDT.apk" 
+            <a href="https://github.com/maxim-saplin/CrossPlatformDiskTest/releases/download/2.2.7/com.Saplin.CPDT.apk" 
             target="_blank">
                 <img src={apk}/>
                 <br/>.APK
@@ -32,6 +33,12 @@ export default function DownloadLinks(props){
             target="_blank">
                 <img src={github}/>
                 <br/>GitHub
+            </a>
+            <a href="https://github.com/maxim-saplin/NetCoreStorageSpeedTest/blob/master/README.md" 
+            style={{opacity: 0.65}}
+            target="_blank">
+                <img src={linux}/>
+                <br/>Linux
             </a>
         </>
     )
