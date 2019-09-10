66e0bd938f63f6d761d19d5d52aece7f5975b016
sdfsfd <sdfd@sdfsd.ff>
2019-09-10 10:27:31 +0300

    Updates

---
+++ precache-manifest.dad50197c1d5f36aa9946f7302192411.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a385cda4344b9f23c7c4cc4cfbd3ea",
+    "url": "/cpdt_results/index.html"
+  },
+  {
+    "revision": "32216b4b4e16726dca8f",
+    "url": "/cpdt_results/static/css/main.a2ddb69e.chunk.css"
+  },
+  {
+    "revision": "48819ac5e0f26e6366fa",
+    "url": "/cpdt_results/static/js/2.7240691e.chunk.js"
+  },
+  {
+    "revision": "32216b4b4e16726dca8f",
+    "url": "/cpdt_results/static/js/main.35ea8952.chunk.js"
+  },
+  {
+    "revision": "8b5e12a7f9d97e85cea8",
+    "url": "/cpdt_results/static/js/runtime~main.dd0a447d.js"
+  },
+  {
+    "revision": "dd31905cb4394b5965adae9cb2bfd25c",
+    "url": "/cpdt_results/static/media/EnBlack.dd31905c.png"
+  },
+  {
+    "revision": "bebad3b9bf225cdecc7b9fe714f3b46d",
+    "url": "/cpdt_results/static/media/EnWhite.bebad3b9.png"
+  },
+  {
+    "revision": "036d007395279645d7dd332ac08e6c66",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Italic.036d0073.woff2"
+  },
+  {
+    "revision": "97aeea180bd7d5694ecdb8998ea9d275",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Italic.97aeea18.woff"
+  },
+  {
+    "revision": "70b59c39c9071242baa75908c9796d0e",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Medium.70b59c39.woff"
+  },
+  {
+    "revision": "d55f91bf0056794f4d2ad74327011e67",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Medium.d55f91bf.woff2"
+  },
+  {
+    "revision": "27a3afd6a029d53608f80f2ee809f614",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Regular.27a3afd6.woff2"
+  },
+  {
+    "revision": "5632af57e4d486d7597865250c6b1e77",
+    "url": "/cpdt_results/static/media/IBMPlexMono-Regular.5632af57.woff"
+  },
+  {
+    "revision": "37698cdd304ef06da08b86889e5593b5",
+    "url": "/cpdt_results/static/media/IBMPlexMono-SemiBold.37698cdd.woff"
+  },
+  {
+    "revision": "6624977e05316ed6e27ee2a32738ab7e",
+    "url": "/cpdt_results/static/media/IBMPlexMono-SemiBold.6624977e.woff2"
+  },
+  {
+    "revision": "bd379b4108d626229acca4a4cf8f66b6",
+    "url": "/cpdt_results/static/media/RuBlack.bd379b41.png"
+  },
+  {
+    "revision": "47b2b792238f5cf6b4a26b91f1e65456",
+    "url": "/cpdt_results/static/media/RuWhite.47b2b792.png"
+  },
+  {
+    "revision": "a00fa3a4c76271f16b9808841091a2b6",
+    "url": "/cpdt_results/static/media/apk.a00fa3a4.svg"
+  },
+  {
+    "revision": "990c2b02425e4ee8299a18b645f267b5",
+    "url": "/cpdt_results/static/media/github.990c2b02.svg"
+  },
+  {
+    "revision": "7041ae46fb80f1678b2c020235176f33",
+    "url": "/cpdt_results/static/media/google-play.7041ae46.svg"
+  },
+  {
+    "revision": "51ffa359ccbd41bedabbf079148d7858",
+    "url": "/cpdt_results/static/media/linux.51ffa359.svg"
+  },
+  {
+    "revision": "3a236d88e7bd2ce0636f60bd1dcc5b7d",
+    "url": "/cpdt_results/static/media/macos.3a236d88.svg"
+  },
+  {
+    "revision": "15e725319135ac27002d0751851a978b",
+    "url": "/cpdt_results/static/media/windows.15e72531.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cpdt_results/precache-manifest.28e671ede58bf036b81a44f5775f84e2.js"
+  "/cpdt_results/precache-manifest.dad50197c1d5f36aa9946f7302192411.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cpdt_results/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cpdt_results/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
