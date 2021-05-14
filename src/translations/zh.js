3d5e369fdb9cc5a2ad64859afeee3328f8290cdb
SummonHIM <33192611+SummonHIM@users.noreply.github.com>
2021-05-14 21:07:37 +0800

    Upload Chinese translate for cpdt_results

---
+++ src/translations/zh.js
@@ -0,0 +1,53 @@
+const translations = 
+{
+    pageTitle: "跨平台硬盘测试：硬盘性能排行榜",
+    aboutTitle:  "跨平台硬盘测试：关于本程序",
+    downloadTitle:  "跨平台硬盘测试：下载",
+    title: "固态/机械硬盘基准测试",
+    subTitle: "排行榜",
+    seqWrite: "[S]顺序写入",
+    seqRead: "[S]顺序读取",
+    randWrite: "[R]随机写入（4KB）",
+    randRead: "[R]随机读取（4KB）",
+    memCopy: "[M]内存复制",
+
+    seqWrite_s: "[S]顺序写入",
+    seqRead_s: "[S]顺序读取",
+    randWrite_s: "[R]随机写入",
+    randRead_s: "[R]随机读取",
+    memCopy_s: "[M]内存复制",
+
+    seqWrite_f: "顺序写入",
+    seqRead_f: "顺序读取",
+    randWrite_f: "随机写入（4KB）",
+    randRead_f: "随机读取（4KB）",
+    memCopy_f: "内存复制",
+
+    throughput: "吞吐量",
+    nothingToShow: "这里啥都没有",
+
+    Android: "[A]ndroid",
+    macOS: "mac[O]S",
+    iOS: "[i]OS",
+    Windows: "[W]indows",
+    all: "全选",
+    mbps: "MB/s",
+    gbps: "GB/s",
+
+    hitEnter: "单击回车键以置顶该记录",
+    hintDoubleTap: "双击以置顶该记录",
+    hintSwipe: "左右滑动以更换测试项",
+    close: "[C]关闭",
+    yourDevice: "您的设备",
+
+    result_SRW: "顺序写入÷读取",
+    result_RRW: "随机写入÷读取",
+    result_MEM: "内存复制",
+
+    result_SW: "顺序写入",
+    result_SR: "顺序读取",
+    result_RW: "随机写入",
+    result_RR: "随机读取"
+}
+
+export default translations;
