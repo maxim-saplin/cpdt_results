10d46758fef60cc10d37c58e625c10808be713b8
SummonHIM <33192611+SummonHIM@users.noreply.github.com>
2021-05-21 23:41:37 +0800

    Upload Chinese translate for cpdt_results (#17)
    * Upload Chinese translate for cpdt_results
    
    * Create AboutZh.js
    
    * Add files via upload
    
    * Better about images
    
    * traditional chinese update
    
    * Update AboutZh.js
    
    * Add files via upload
    
    * Create AboutZht.js
    
    * Update AboutZh.js
    
    * Update AboutZht.js
    
    * Applying zh and zht translations
    
    Co-authored-by: sdfsfd <sdfd@sdfsd.ff>

---
+++ src/translations/zht.js
@@ -0,0 +1,53 @@
+const translations = 
+{
+    pageTitle: "跨平臺硬碟測試：硬碟效能排行榜",
+    aboutTitle:  "跨平臺硬碟測試：關於本程式",
+    downloadTitle:  "跨平臺硬碟測試：下載",
+    title: "固態/機械硬碟基準測試",
+    subTitle: "排行榜",
+    seqWrite: "[S]順序寫入",
+    seqRead: "[S]順序讀取",
+    randWrite: "[R]隨機寫入（4KB）",
+    randRead: "[R]隨機讀取（4KB）",
+    memCopy: "[M]記憶體複製",
+
+    seqWrite_s: "[S]順序寫入",
+    seqRead_s: "[S]順序讀取",
+    randWrite_s: "[R]隨機寫入",
+    randRead_s: "[R]隨機讀取",
+    memCopy_s: "[M]記憶體複製",
+
+    seqWrite_f: "順序寫入",
+    seqRead_f: "順序讀取",
+    randWrite_f: "隨機寫入（4KB）",
+    randRead_f: "隨機讀取（4KB）",
+    memCopy_f: "記憶體複製",
+
+    throughput: "吞吐量",
+    nothingToShow: "這裡啥都沒有",
+
+    Android: "[A]ndroid",
+    macOS: "mac[O]S",
+    iOS: "[i]OS",
+    Windows: "[W]indows",
+    all: "全選",
+    mbps: "MB/s",
+    gbps: "GB/s",
+
+    hitEnter: "單擊回車鍵以置頂該記錄",
+    hintDoubleTap: "雙擊以置頂該記錄",
+    hintSwipe: "左右滑動以更換測試項",
+    close: "[C]關閉",
+    yourDevice: "您的裝置",
+
+    result_SRW: "順序寫入÷讀取",
+    result_RRW: "隨機寫入÷讀取",
+    result_MEM: "記憶體複製",
+
+    result_SW: "順序寫入",
+    result_SR: "順序讀取",
+    result_RW: "隨機寫入",
+    result_RR: "隨機讀取"
+}
+
+export default translations;
